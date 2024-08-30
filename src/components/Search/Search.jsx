02b5f68df4830678e5fa9b81857ebd2ea712dea7
kvineet07 <kumarvineet3445@gmail.com>
2024-08-30 19:58:29 +0530

    qtify deployement 2

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import styles from "./Search.module.css";
-import { ReactComponent as SearchIcon } from "../../assets/Search-icon.svg";
+import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 
 export default function Search({ placeholder }) {
   const handleSubmit = (e) => {
